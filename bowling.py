067477091daaab7430df7946d7b9bf010073c72c
Gergely Kovacs <gergelyk89@gmail.com>
2017-11-29 10:01:11 +0100

    First run at clean code, new function for 'spare_score', get_value char searches in one string, some positional fixes

--- bowling.py
+++ bowling.py
@@ -1,43 +1,47 @@
+def spare_score(current_throw, last_throw):
+
+    if current_throw == '/':
+        return 10 - get_value(last_throw)
+    else:
+        return get_value(current_throw)
+
+
 def score(game):
+
     result = 0
     frame = 1
     in_first_half = True
     for i in range(len(game)):
+
+        result += spare_score(game[i], game[i-1])
+
         if game[i] == '/':
-            result += 10 - last
-        else:
-            result += get_value(game[i])
-        # if not in_first_half:
-            # frame += 1
-        if frame < 10  and get_value(game[i]) == 10:
-            if game[i] == '/':
+            result += get_value(game[i+1])
+
+        if frame < 10 and get_value(game[i]) == 10:
+            if game[i] == 'X' or game[i] == 'x':
                 result += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
-                if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
-                else:
-                    result += get_value(game[i+2])
-        last = get_value(game[i])
+                result += spare_score(game[i+2], game[i+1])
+
         if not in_first_half:
             frame += 1
-        if in_first_half == True:
+
+        if in_first_half:
             in_first_half = False
         else:
-            in_first_half = True
+            in_first_half = not in_first_half
+
         if game[i] == 'X' or game[i] == 'x':
             in_first_half = True
             frame += 1
     return result
 
+
 def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
+    
+    if char in '123456789':
         return int(char)
-    elif char == 'X' or char == 'x':
-        return 10
-    elif char == '/':
+    elif char.upper() == 'X' or char == '/':
         return 10
     elif char == '-':
         return 0
