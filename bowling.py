c26bec98844460075e800dbc30bec1e8e07c2663
Gergely Kovacs <gergelyk89@gmail.com>
2017-12-03 15:27:04 +0100

    -one deeper conditional, bit less duplication, nicer format

--- bowling.py
+++ bowling.py
@@ -1,5 +1,4 @@
 def spare_score(current_throw, last_throw):
-
     if current_throw == '/':
         return 10 - get_value(last_throw)
     else:
@@ -7,41 +6,35 @@ def spare_score(current_throw, last_throw):
 
 
 def score(game):
-
     result = 0
     frame = 1
     in_first_half = True
+
     for i in range(len(game)):
 
         result += spare_score(game[i], game[i-1])
 
-        if game[i] == '/':
-            result += get_value(game[i+1])
-
         if frame < 10 and get_value(game[i]) == 10:
+            result += get_value(game[i+1])
             if game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
                 result += spare_score(game[i+2], game[i+1])
 
         if not in_first_half:
             frame += 1
 
-        if in_first_half:
-            in_first_half = False
-        else:
-            in_first_half = not in_first_half
+        in_first_half = not in_first_half
 
         if game[i] == 'X' or game[i] == 'x':
             in_first_half = True
             frame += 1
+
     return result
 
 
 def get_value(char):
-    
-    if char in '123456789':
+    if char in "123456789":
         return int(char)
-    elif char.upper() == 'X' or char == '/':
+    elif char.upper() in 'X/':
         return 10
     elif char == '-':
         return 0
